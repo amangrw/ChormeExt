2d7355b276dead1462c3d59dfca73106c5447f3a
amangrw <39518200+amangrw@users.noreply.github.com>
2020-06-19 20:47:21 +0530

    Update popup.js

--- myexten/popup.js
+++ myexten/popup.js
@@ -1,22 +1,35 @@
 chrome.runtime.onInstalled.addListener(function() {
-		chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
 	        chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
 		        if (change.url) {
 			        let data = change.url;
 			        let currentDateTime = Date()
-
-	                localStorage.setItem(currentDateTime , data);
+	                	localStorage.setItem(currentDateTime , data);
 		        }
-		    });
+		 });
 
 
-		}); 
+	}); 
  });
 
 let data = Object.entries(localStorage);
-console.log(data);
 
 document.addEventListener('DOMContentLoaded', function() {
 	let isOutput = document.getElementById('IsOutput');
-	isOutput.innerHTML = data;
+	if (isOutput != null){
+		isOutput.innerHTML = data;
+	}
 });
+
+for (var i = 0; i < data.length; i++) { // the plainest of array loops
+  var obj = data[i];
+  // for..in object iteration will set the key for each pair
+  // and the value is in obj[key]
+  for (var key in obj) { 
+  	console.log(JSON.stringify(data));
+    console.log(key, obj[key]);
+
+  }
+}
+
+ 
