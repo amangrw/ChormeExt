70677be98a1031446fb8949e8376c4c76c3f49df
amangrw <39518200+amangrw@users.noreply.github.com>
2020-06-22 19:43:45 +0530

    Update popup.js

--- myexten/popup.js
+++ myexten/popup.js
@@ -1,33 +1,41 @@
+let data = Object.entries(localStorage);
+let all_url;
+
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+		chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
 	        chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
 		        if (change.url) {
 			        let data = change.url;
 			        let currentDateTime = Date()
-	                	localStorage.setItem(currentDateTime , data);
+
+	                localStorage.setItem(currentDateTime , data);
 		        }
-		 });
+		    });
 
 
-	}); 
+		}); 
  });
-
-let data = Object.entries(localStorage);
 
 document.addEventListener('DOMContentLoaded', function() {
 	let isOutput = document.getElementById('IsOutput');
 	if (isOutput != null){
-		isOutput.innerHTML = data;
+		for (var i = 0; i < data.length; i++) {
+		  var obj = data[i];
+		  isOutput.innerHTML = isOutput.innerHTML +"<tr><td style='width:20px;'>"+(i+1)+"</td><td>" + obj[0] + "</td><td class='urls'><a target='_blank' href='"+ obj[1]+ "'>" + obj[1] + "</a></td></tr>";
+		}
 	}
 });
 
-for (var i = 0; i < data.length; i++) { // the plainest of array loops
-  var obj = data[i];
-  // for..in object iteration will set the key for each pair
-  // and the value is in obj[key]
-  for (var key in obj) { 
-    	console.log(key, obj[key]);
-  }
+
+for (var i = 0; i < data.length; i++) { 
+   var obj = data[i];
+   all_url = all_url + obj[1] + ",";
+
 }
+console.log(all_url	)
+
+
+  
+
 
  
