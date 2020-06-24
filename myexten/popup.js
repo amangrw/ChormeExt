227bb18d29dafccd2417e1f180509d45585cff6c
amangrw <39518200+amangrw@users.noreply.github.com>
2020-06-24 22:32:05 +0530

    Update popup.js

--- myexten/popup.js
+++ myexten/popup.js
@@ -1,21 +1,40 @@
 let data = Object.entries(localStorage);
+let globalcurrentUrl;
 let all_url;
+let counts = {};
 
+//fetching and saving the all urls in localStorage
 chrome.runtime.onInstalled.addListener(function() {
 		chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
 	        chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
 		        if (change.url) {
-			        let data = change.url;
-			        let currentDateTime = Date()
-
-	                localStorage.setItem(currentDateTime , data);
+			        let currentDateTime = Date();
+			        let currentUrl = change.url;
+	                localStorage.setItem(currentDateTime , currentUrl);
 		        }
+		        var counts = {};
+				for (var i = 0; i < all_url.length; i++) {
+				  var num = arr[i];
+				  counts[num] = counts[num] ? counts[num] + 1 : 1;
+				}
+				console.log(counts[currentUrl]);
 		    });
-
-
 		}); 
  });
 
+
+chrome.runtime.onInstalled.addListener(function() {
+	window.onbeforeunload = function () {
+	    return alert("Do you really want to close?");
+	};
+});
+
+function setValue(currentUrl){
+	globalcurrentUrl = currentUrl ;
+	console.log(globalcurrentUrl);
+}
+
+//showing the whole data from localStorage 
 document.addEventListener('DOMContentLoaded', function() {
 	let isOutput = document.getElementById('IsOutput');
 	if (isOutput != null){
@@ -28,11 +47,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 for (var i = 0; i < data.length; i++) { 
-   var obj = data[i];
-   all_url = all_url + obj[1] + ",";
+	var obj = data[i];
+	all_url = all_url + obj[1] + ",";
 
 }
-console.log(all_url	)
+
+//console.log(globalcurrentUrl);
+
+
 
 
   
