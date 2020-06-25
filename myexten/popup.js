9b979a3a29c6fced7aa05a912ad6d5b1315ca2fe
amangrw <39518200+amangrw@users.noreply.github.com>
2020-06-25 14:05:35 +0530

    Update popup.js

--- myexten/popup.js
+++ myexten/popup.js
@@ -1,28 +1,34 @@
 let data = Object.entries(localStorage);
-let globalcurrentUrl;
-let all_url;
-let counts = {};
+let all_url =[]	;
 
-//fetching and saving the all urls in localStorage
+
+for (var i = 0; i < data.length; i++) { 
+	var obj = data[i];
+	all_url[i] = obj[1];
+
+}
+
 chrome.runtime.onInstalled.addListener(function() {
 		chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
 	        chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
 		        if (change.url) {
 			        let currentDateTime = Date();
 			        let currentUrl = change.url;
-	                	localStorage.setItem(currentDateTime , currentUrl);
-			        var counts = {};
-				for (var i = 0; i < all_url.length; i++) {
-				    var num = all_url[i];
-				    counts[num] = counts[num] ? counts[num] + 1 : 1;
-				};
-				alert(counts[currentUrl]);
-				console.log(counts[currentUrl]);
+				localStorage.setItem(currentDateTime , currentUrl, tabId);
+				if (all_url != null){
+					var counts = {};
+					for (var i = 0; i < all_url.length; i++) {
+						var num = all_url[i];
+						counts[num] = counts[num] ? counts[num] + 1 : 1;
+					}	
+					if( counts[change.url] >= 10 ){
+						alert(counts[change.url]+ " times you visited this url : "+ change.url);							
+					}
+				}
 		        }
 		    });
 		}); 
  });
-
 
 chrome.runtime.onInstalled.addListener(function() {
 	window.onbeforeunload = function () {
@@ -30,10 +36,6 @@ chrome.runtime.onInstalled.addListener(function() {
 	};
 });
 
-// function setValue(currentUrl){
-// 	globalcurrentUrl = currentUrl ;
-// 	console.log(globalcurrentUrl);
-// };
 
 //showing the whole data from localStorage 
 document.addEventListener('DOMContentLoaded', function() {
@@ -47,13 +49,6 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-for (var i = 0; i < data.length; i++) { 
-	var obj = data[i];
-	all_url = all_url + obj[1] + ",";
-
-}
-
-//console.log(globalcurrentUrl);
 
 
 
