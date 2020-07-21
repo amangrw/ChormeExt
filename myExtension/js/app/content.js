34d2a06377c136c1b88cf220b676204ea73ca277
amangrw <39518200+amangrw@users.noreply.github.com>
2020-07-21 21:08:03 +0530

    added html element and calling ajax

--- myExtension/js/app/content.js
+++ myExtension/js/app/content.js
@@ -1,4 +1,4 @@
-// ﻿//alert('content script loaded');
+// //alert('content script loaded');
 
 // chrome.extension.onMessage.addListener(
 // function (request, sender, sendResponse) {
@@ -27,9 +27,29 @@
 // });
 
 
+
 // var body = document.getElementByTagName('body');
 // body.innerHTML = "<button>click</button>"
 
 var div=document.createElement("div"); 
 document.body.appendChild(div); 
-div.innerHTML="<a href="#">test123</a>";
+div.innerHTML="<div ng-app='myExtension' ng-csp><div class='suggestions' ng-controller='suggestionController'><div ng-repeat='suggestion in suggestions'><label class='suggestions-label'>e-commerce</label></div></div></div>";
+
+myApp.controller("suggestionController", function($scope, $http){
+    $scope.suggestions = [];
+    $scope.load = function(){
+            $http({
+                url: ' http://localhost:3000/suggestion',
+                method: "GET",
+                data: $scope.suggestions
+            })
+            .then(function(response) {
+                // success
+                $scope.suggestions = response.data;
+            }, 
+            function(response) { // optional
+                // failed
+                alert("unsuccessfull")
+            });
+        }
+});
