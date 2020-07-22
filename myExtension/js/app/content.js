5af5cded3c8a503417f3af6996bc4e6a0f6b66c9
amangrw <39518200+amangrw@users.noreply.github.com>
2020-07-22 17:27:47 +0530

    Update content.js

--- myExtension/js/app/content.js
+++ myExtension/js/app/content.js
@@ -32,8 +32,17 @@
 // body.innerHTML = "<button>click</button>"
 
 var div=document.createElement("div"); 
-document.body.appendChild(div); 
+document.getElementsByClassName("result")[0].appendChild(div);
+document.querySelector(".result").appendChild(div);
+//document.getElementsByClassName("result").appendChild(div);
 div.innerHTML="<div ng-app='myExtension' ng-csp><div class='suggestions' ng-controller='suggestionController'><div ng-repeat='suggestion in suggestions'><label class='suggestions-label'>e-commerce</label></div></div></div>";
+
+
+  // var node = document.createElement("div");
+  // var textnode = document.createTextNode("Water");
+  // node.appendChild(textnode);
+  // document.getElementById("myList").appendChild(node);
+
 
 myApp.controller("suggestionController", function($scope, $http){
     $scope.suggestions = [];
