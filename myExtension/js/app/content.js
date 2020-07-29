a2838073d78d90cff4eddca349e61704781f4702
amangrw <39518200+amangrw@users.noreply.github.com>
2020-07-29 19:18:08 +0530

    getting data from api

--- myExtension/js/app/content.js
+++ myExtension/js/app/content.js
@@ -39,11 +39,27 @@
 
 
 var result = document.getElementsByClassName("result");
+// var link = document.getElementsByClassName("js-result-title");
 
 for (var i = 0; i < result.length; i++) {
+	var link = document.getElementsByClassName("js-result-title")[i];
 	var div=document.createElement("div"); 
-	div.innerHTML="<div class='suggestions'><label class='suggestions-label'>e-commerce</label></div>";
+	div.innerHTML="<div class='suggestions'><label class='suggestions-label'>"+link.href+"</label></div>";
 	result[i].appendChild(div);
+	//console.log(link[i].href);
+	getInformation(link);
+}
+
+function getInformation(link) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var data = this.responseText
+      console.log(data.categories[0])
+    }
+  };
+  xhttp.open("GET", "https://website-categorization.whoisxmlapi.com/api/v1?apiKey=at_zs6tishK2xEXWG5TdY1uy7htcOPS3&domainName=" + link, true);
+  xhttp.send();
 }
 
   // var node = document.createElement("div");
@@ -52,22 +68,22 @@ for (var i = 0; i < result.length; i++) {
   // document.getElementById("myList").appendChild(node);
 
 
-myApp.controller("suggestionController", function($scope, $http){
-    $scope.suggestions = [];
-    $scope.load = function(){
-            $http({
-                url: ' http://localhost:3000/suggestion',
-                method: "GET",
-                data: $scope.suggestions
-            })
-            .then(function(response) {
-                // success
-                $scope.suggestions = response.data;
-                alert("hi");
-            }, 
-            function(response) { // optional
-                // failed
-                alert("unsuccessfull")
-            });
-        }
-})
+// myApp.controller("suggestionController", function($scope, $http){
+//     $scope.suggestions = [];
+//     $scope.load = function(){
+//             $http({
+//                 url: 'https://website-categorization.whoisxmlapi.com/api/v1?apiKey=at_zs6tishK2xEXWG5TdY1uy7htcOPS3&domainName=bbc.com',
+//                 method: "GET",
+//                 data: $scope.suggestions
+//             })
+//             .then(function(response) {
+//                 // success
+//                 $scope.suggestions = response.data;
+//                 alert("hi");
+//             }, 
+//             function(response) { // optional
+//                 // failed
+//                 alert("unsuccessfull")
+//             });
+//         }
+// })
