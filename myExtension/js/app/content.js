6fe6ca6448552c86553481d5f4d435d731235061
amangrw <39518200+amangrw@users.noreply.github.com>
2020-08-26 23:46:40 +0530

    added some button for makemydrivefun website

--- myExtension/js/app/content.js
+++ myExtension/js/app/content.js
@@ -1,14 +1,28 @@
+var categorizationApi ="https://website-categorization.whoisxmlapi.com/api/v1?apiKey=at_zs6tishK2xEXWG5TdY1uy7htcOPS3&domainName="
 var result = document.getElementsByClassName("result");
-// var link = document.getElementsByClassName("js-result-title");
+var drive = document.getElementsByClassName("paper");
 
 for (var i = 0; i < result.length; i++) {
 	var link = document.getElementsByClassName("js-result-title")[i];
-	getInformation(link, i);
+	getInformation(link, i, categorizationApi);
 }
 
-function getInformation(link, i) {
+var drive = document.getElementsByClassName("paper")[0];
+addElement(drive);
+console.log("hi")
+
+
+function setElement(data, link, i){
+      //console.log(data)
+  var div=document.createElement("div"); 
+  div.innerHTML="<div class='suggestions'><label class='suggestions-label'>"+data+"</label></div>";
+  result[i].appendChild(div);
+}
+
+
+function getInformation(link, i, linkApi) {
   var xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "https://website-categorization.whoisxmlapi.com/api/v1?apiKey=at_zs6tishK2xEXWG5TdY1uy7htcOPS3&domainName=" + link, true);
+  xhttp.open("GET", linkApi + link, true);
   xhttp.send();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -19,10 +33,19 @@ function getInformation(link, i) {
 }
 
 function setlabel(data, link, i){
-      console.log(data)
+      //console.log(data)
   var div=document.createElement("div"); 
   div.innerHTML="<div class='suggestions'><label class='suggestions-label'>"+data+"</label></div>";
   result[i].appendChild(div);
 }
+
+
+function addElement(drive){
+  var div=document.createElement("div");
+  div.innerHTML="<div><button style='background:#FD6C6C'>Filter</button><button style='background:#FD6C6C'>Button1</button><button style='background:#FD6C6C'>Button2</button></div>";
+  drive.prepend(div);
+}
+
+
 
  
